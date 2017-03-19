838db3ce3bd0648dcabde6165120fc58369f5152
gogu <gogu9909@gmail.com>
2017-03-19 16:54:33 +0800

    Updated web components with crud rest api.

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -5,28 +5,81 @@
   const Main = {
     data() {
       return {
-        bullets: records.getLocalBullets(),
-        editingBullet: initRecord()
+        rawBullets: [],
+        editingBullet: initRecord(),
+        today: moment().format('YYYY/M/D/dd'),
+        dayRange: 2,
       }
     },
+    computed: {
+      bulletsInDays() {
+        return this.rawBullets.reduce((prev, record) => {
+          const day = moment.unix(record.timestamp).format('YYYY/M/D/dd')
+          prev[day] = prev[day] || []
+          record.day = day
+          prev[day].push(record)
+          return prev
+        }, {})
+      },
+      alldays() {
+        const days = Object.keys(this.bulletsInDays).sort()
+        if (days.indexOf(this.today) > -1) return days
+        return days.concat(this.today)
+      },
+      days() {
+        return this.alldays.slice(-this.dayRange)
+      },
+    },
     mounted() {
+      records.getBullets().then(bullets => {
+        this.rawBullets = bullets
+      })
+      setTimeout(() => {
+        window.scroll(0, document.body.clientHeight)
+      }, 300)
     },
     methods: {
       addBullet(record) {
-        const updated = records.addBullet(record)
-        this.bullets = updated
-        this.editingBullet = initRecord()
+        records.addBullet(record).then(updated => {
+          console.log(updated)
+          this.rawBullets = updated
+          this.editingBullet = initRecord()
+          this.$nextTick(() => {
+            window.scroll(0, document.body.clientHeight)
+            this.$refs.editingInput.focus()
+          })
+        })
       },
       setBullet(record) {
-        const updated = records.setBullet(record)
-        this.bullelts = updated
+        records.setBullet(record).then(updated => {
+          this.rawBullets = updated
+        })
+      },
+      removeBullet(record) {
+        records.removeBullet(record).then(updated => {
+          this.rawBullets = updated
+        })
+      },
+      showPrev() {
+        this.dayRange += 1
       },
     },
     template: `
-      <section>
-        <h1>One Shot Demo: Bullet Component</h1>
-        <bullet-input v-for="bullet of bullets" :key="bullets.id" :bullet="bullet" @submit="setBullet"></bullet-input>
-        <bullet-input :bullet="editingBullet" @submit="addBullet"></bullet-input>
+      <section class="appcontainer">
+        <button class="prevbutton" v-if="dayRange < alldays.length" @click="showPrev">Prev</button>
+        <transition-group name="slide-fade">
+          <div v-for="day of days" :key="day">
+            <h4>{{ day }}</h4>
+            <bullet-input v-for="bullet of bulletsInDays[day]"
+              :key="bullet.id"
+              :bullet="bullet"
+              @submit="setBullet"
+              @delete="removeBullet"
+              :readonly="bullet.day !== today"></bullet-input>
+          </div>
+        </transition-group>
+        <h4 v-if="days.indexOf(today) < 0">{{ today }}</h4>
+        <bullet-input ref="editingInput" :bullet="editingBullet" @submit="addBullet"></bullet-input>
       </section>
     `,
   }
